a512eb509e34182ca1dd8635bf82d9c802a4063a
kimsuhoe01-creator <kimsuhoe01@gmail.com>
2026-08-30 23:04:33 +0700

    Make CUKCUK self-order the live default

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'dabang-tablet-v24';
+const CACHE_VERSION = 'dabang-tablet-v25';
 const SHELL_CACHE = `${CACHE_VERSION}-shell`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 const APP_SHELL = [
